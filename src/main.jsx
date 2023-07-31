9f9f5c3f1714d89db17a522414ff124e43a9c9af
Kgotso Makhalimele <kgotsom@protonmail.com>
2023-07-31 15:06:48 +0200

    Reducer Hot Loading added

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import {Provider} from "react-redux";
 import configureStore from "./store/configureStore.js";
+import process from "../.eslintrc.cjs";
 
 const store = configureStore()
+const rootEl = document.getElementById('root')
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <Provider store={store}>
-          <App />
-      </Provider>
-  </React.StrictMode>,
-)
+let render = () => {
+    const App = require('./App.jsx').default
+    ReactDOM.render(
+        <React.StrictMode>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </React.StrictMode>,
+        rootEl,
+    )
+}
+
+if (process.env.NODE_ENV !== 'production') {
+    // Whenever the App component file or one of its dependencies
+    // is changed, re-import the updated component and re-render it
+    if(module.hot) {
+        module.hot.accept('./App.jsx', () => setTimeout(render))
+    }
+}
+
+render()
